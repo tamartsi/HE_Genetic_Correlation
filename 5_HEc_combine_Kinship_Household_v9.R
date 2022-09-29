15e5b7dbc0c26c5f35c71ee5a339e1129adb8bd5
elgartmi <elgart@gmail.com>
2022-09-29 21:04:31 +0300

    Update 5_HEc_combine_Kinship_Household_v9.R

--- 5_HEc_combine_Kinship_Household_v9.R
+++ 5_HEc_combine_Kinship_Household_v9.R
@@ -20,7 +20,7 @@ phenotypes <- read.table(pheno_names,sep="\t",header = F, row.names = 1)
 phenotypes <- phenotypes[[1]]
 
 VCg1 <- rep(NA,length(phenotypes));names(VCg1) <- phenotypes
-VCg2 <- rep(NA,length(phenotypes));names(VCg2) <- phenotypes
+VCg2 <- rep(0,length(phenotypes));names(VCg2) <- phenotypes
 VCe <- rep(NA,length(phenotypes));names(VCe) <- phenotypes
 
 res <- list()
@@ -33,37 +33,64 @@ for(i in 1:length(phenotypes)){ # i=1
     next
   }
   data = readLines(f)
-  rsG1 <- data[which(grepl("V\\(G1)\t", data))]
-  if(length(rsG1)!=1){
-    print("Something is wrong")
-    break
+  if(length(config$household_matrix_filename)>0){ #two matrix case
+    rsG1 <- data[which(grepl("V\\(G1)\t", data))]
+    if(length(rsG1)!=1){
+      print("Something is wrong")
+      break
+    }
+    rsG2 <- data[which(grepl("V\\(G2)\t", data))]
+    if(length(rsG2)!=1){
+      print("Something is wrong")
+      break
+    }
+    rsE <- data[which(grepl("V\\(e)\t", data))]
+    if(length(rsE)!=1){
+      print("Something is wrong")
+      break
+    }
+    rsN <- data[which(grepl("n\t", data))]
+    if(length(rsN)!=1){
+      print("Something is wrong")
+      break
+    }
+    mnG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][2])
+    seG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][3])
+    mnG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][2])
+    seG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][3])
+    mnE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][2])
+    seE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][3])
+    nn <- as.numeric(strsplit(rsN,"\t",fixed = T)[[1]][2])
+    
+    VCg1[[pheno.1]] <- mnG1 / (mnG1 + mnG2 + mnE)
+    VCg2[[pheno.1]] <- mnG2 / (mnG1 + mnG2 + mnE)
+    VCe[[pheno.1]] <- mnE / (mnG1 + mnG2 + mnE)
+  }else{
+    rsG <- data[which(grepl("V\\(G)\t", data))]
+    if(length(rsG)!=1){
+      print("Something is wrong")
+      break
+    }
+    rsE <- data[which(grepl("V\\(e)\t", data))]
+    if(length(rsE)!=1){
+      print("Something is wrong")
+      break
+    }
+    rsN <- data[which(grepl("n\t", data))]
+    if(length(rsN)!=1){
+      print("Something is wrong")
+      break
+    }
+    mnG <- as.numeric(strsplit(rsG,"\t",fixed = T)[[1]][2])
+    seG <- as.numeric(strsplit(rsG,"\t",fixed = T)[[1]][3])
+    mnE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][2])
+    seE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][3])
+    nn <- as.numeric(strsplit(rsN,"\t",fixed = T)[[1]][2])
+    
+    VCg1[[pheno.1]] <- mnG/(mnG+mnE)
+    VCe[[pheno.1]] <- mnE/(mnG+mnE)
+    
   }
-  rsG2 <- data[which(grepl("V\\(G2)\t", data))]
-  if(length(rsG2)!=1){
-    print("Something is wrong")
-    break
-  }
-  rsE <- data[which(grepl("V\\(e)\t", data))]
-  if(length(rsE)!=1){
-    print("Something is wrong")
-    break
-  }
-  rsN <- data[which(grepl("n\t", data))]
-  if(length(rsN)!=1){
-    print("Something is wrong")
-    break
-  }
-  mnG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][2])
-  seG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][3])
-  mnG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][2])
-  seG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][3])
-  mnE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][2])
-  seE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][3])
-  nn <- as.numeric(strsplit(rsN,"\t",fixed = T)[[1]][2])
-  
-  VCg1[[pheno.1]] <- mnG1 / (mnG1 + mnG2 + mnE)
-  VCg2[[pheno.1]] <- mnG2 / (mnG1 + mnG2 + mnE)
-  VCe[[pheno.1]] <- mnE / (mnG1 + mnG2 + mnE)
 }
 
 #read all chunks, recalculate NormK, NormHH with the GCTA VCs
@@ -84,27 +111,38 @@ ofilename = paste0(outfileprefix,"_combine.txt")
 print("Loading Phenotypes...")
 phenotypes <- readRDS(config$phenotypes_filename)
 rownames(phenotypes) <- phenotypes[[config$idfield_name]]
-phenotypes <- phenotypes[,c(config$idfield_name,covariates,config$phenotypes_fieldnames)]
+
+if((length(config$phenotypes_fieldnames)==1) & (length(grep('-',config$phenotypes_fieldnames))==1)){   #10-100
+  sst <- as.numeric(strsplit(config$phenotypes_fieldnames,"-")[[1]][1])
+  sse <- as.numeric(strsplit(config$phenotypes_fieldnames,"-")[[1]][2])
+  phenotypes_fieldnames <- colnames(phenotypes)[sst:sse]
+}else{
+  phenotypes_fieldnames <- config$phenotypes_fieldnames
+}
+
+phenotypes <- phenotypes[,c(config$idfield_name,covariates,phenotypes_fieldnames)]
 
 print("Loading relatedness matrices...")
 mat_phi <- readRDS(config$kinship_matrix_filename)
-mat_hh <- readRDS(config$household_matrix_filename)
-
 cn <- colnames(mat_phi);cn <- cn[!is.na(cn)]
-cn <- cn[cn %in% colnames(mat_hh)]
 cn <- cn[cn %in% phenotypes[[IDFIELD]]]
+
+if(length(config$household_matrix_filename)>0){
+  mat_hh <- readRDS(config$household_matrix_filename)
+  cn <- cn[cn %in% colnames(mat_hh)]
+}
 
 rownames(phenotypes) <- phenotypes[[IDFIELD]]
 pdata <- phenotypes[cn,]
 
 
-phenotypes<-config$phenotypes_fieldnames
+phenotypes<-phenotypes_fieldnames
 tpname <- paste0(workdirname,outfileprefix,"_Phenotypes",length(phenotypes),"_PerBatch",ONEBATCH,"_accessory.RData")
 if(file.exists(tpname)){
   print("Loading accessory data")
   load(tpname)
 }else{
-  stop(paste0("Generating and saving accessory data: ",tpname))
+  stop(paste0("Couldn't find accessory data: ",tpname))
 }
 
 NN <- ceiling(length(accD$tpairs$a) / ONEBATCH)
@@ -142,85 +180,88 @@ for (N in 1:NN){ # N <- 1
   tdata<-as.data.frame(tdata[,2:length(tdata)]);colnames(tdata)<-cn;rownames(tdata)<-rn
   tnames <- names(tdata)
 
-  i=1  
-  pheno.1 <- strsplit(cn[i],"\\.")[[1]][1]
-  pheno.2 <- strsplit(cn[i],"\\.")[[1]][2]
-  print(paste0(pheno.1," - ",pheno.2))
-
-  tK <- as.numeric(tdata["GenCorr",cn[i]])
-  if(is.na(tK)){
-    tNormK <- NA
-  }else if(tK==-100){
-    tNormK <- -100
-  }else{
-    tNormK <- sqrt(VCg1[[pheno.1]]) * sqrt(VCg1[[pheno.2]]) * tK / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
-  }
-  print(paste0(tNormK, " - ", tdata["NormGenCorr",cn[i]]))
-  tdata["NormGenCorr",cn[i]] <- tNormK
-
-  tE <- as.numeric(tdata["ResidCorr",cn[i]])
-  if(is.na(tE)){
-    tNormE <- NA
-  }else if(tE==-100){
-    tNormE <- -100
-  }else{
-    tNormE <- sqrt(VCe[[pheno.1]]) * sqrt(VCe[[pheno.2]]) * tE / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
-  }
-
-  tHH <- as.numeric(tdata["HHCorr",cn[i]])
-  if(is.na(tHH)){
-    tNormHH <- NA
-  }else if(tHH==-100){
-    tNormHH <- -100
-  }else{
-    tNormHH <- sqrt(VCg2[[pheno.1]]) * sqrt(VCg2[[pheno.2]]) * tHH / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
-  }
-  print(paste0(tNormHH, " - ", tdata["NormHHCorr",cn[i]]))
-  tdata["NormHHCorr",cn[i]] <- tNormHH
+  for(i in 1:dim(tdata)[2]){ # i=2
+    pheno.1 <- strsplit(cn[i],"\\.")[[1]][1]
+    pheno.2 <- strsplit(cn[i],"\\.")[[1]][2]
+    print(paste0(pheno.1," - ",pheno.2))
   
-  
-  tcSP <- tNormE + tNormK + tNormHH;
-  if(!is.na(tcSP)){
-    if(tcSP<= -100){
-      tcSP <- -100
+    tK <- as.numeric(tdata["GenCorr",cn[i]])
+    if(is.na(tK)){
+      tNormK <- NA
+    }else if(tK==-100){
+      tNormK <- -100
+    }else{
+      tNormK <- sqrt(VCg1[[pheno.1]]) * sqrt(VCg1[[pheno.2]]) * tK / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
     }
-  }
+    print(paste0(tNormK, " - ", tdata["NormGenCorr",cn[i]]))
+    tdata["NormGenCorr",cn[i]] <- tNormK
   
-  if(is.na(tcSP)){
-    tcSPpval <- NA
-  }else if (tcSP<= -100){
-    tcSPpval <- -100
-  }else{
-    print(tcSP)
-    z <- 0.5 * log((1+tcSP)/(1-tcSP))
-    zse <- 1/sqrt(as.numeric(tdata["n_people",cn[i]])-3)
-    tcSPpval <- min(pnorm(z, sd=zse), pnorm(z, lower.tail=F, sd=zse))*2
-  }
+    tE <- as.numeric(tdata["ResidCorr",cn[i]])
+    if(is.na(tE)){
+      tNormE <- NA
+    }else if(tE==-100){
+      tNormE <- -100
+    }else{
+      tNormE <- sqrt(VCe[[pheno.1]]) * sqrt(VCe[[pheno.2]]) * tE / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
+    }
   
-  tdata["modelPheno",cn[i]] <- tcSP
-  tdata["modelPheno_pval",cn[i]] <- tcSPpval
-  
+    tHH <- as.numeric(tdata["HHCorr",cn[i]])
+    if(is.na(tHH)){
+      tNormHH <- NA
+    }else if(tHH==-100){
+      tNormHH <- -100
+    }else{
+      tNormHH <- sqrt(VCg2[[pheno.1]]) * sqrt(VCg2[[pheno.2]]) * tHH / sqrt((VCg1[[pheno.1]]+VCg2[[pheno.1]]+VCe[[pheno.1]])*(VCg1[[pheno.2]]+VCg2[[pheno.2]]+VCe[[pheno.2]]))
+    }
+    print(paste0(tNormHH, " - ", tdata["NormHHCorr",cn[i]]))
+    tdata["NormHHCorr",cn[i]] <- tNormHH
+    
+    if(length(config$household_matrix_filename)>0){
+      tcSP <- tNormE + tNormK + tNormHH;
+    }else{
+      tcSP <- tNormE + tNormK;
+    }
+    
+    if(!is.na(tcSP)){
+      if(tcSP<= -100){
+        tcSP <- -100
+      }
+    }
+    
+    if(is.na(tcSP)){
+      tcSPpval <- NA
+    }else if (tcSP<= -100){
+      tcSPpval <- -100
+    }else{
+      # print(tcSP)
+      z <- 0.5 * log((1+tcSP)/(1-tcSP))
+      zse <- 1/sqrt(as.numeric(tdata["n_people",cn[i]])-3)
+      tcSPpval <- min(pnorm(z, sd=zse), pnorm(z, lower.tail=F, sd=zse))*2
+    }
+    
+    tdata["modelPheno",cn[i]] <- tcSP
+    tdata["modelPheno_pval",cn[i]] <- tcSPpval
+    
+    i1 <- as.numeric(match(pheno.1,phenotypes))
+    i2 <- as.numeric(match(pheno.2,phenotypes))
+    
+    res2$K[cbind(i1,i2)] <- tdata["GenCorr",cn[i]]; res2$K[cbind(i2,i1)] <- tdata["GenCorr",cn[i]]; 
+    res2$HH[cbind(i1,i2)] <- tdata["HHCorr",cn[i]]; res2$HH[cbind(i2,i1)] <- tdata["HHCorr",cn[i]]; 
+    res2$NormK[cbind(i1,i2)] <- tdata["NormGenCorr",cn[i]]; res2$NormK[cbind(i2,i1)] <- tdata["NormGenCorr",cn[i]]; 
+    res2$NormHH[cbind(i1,i2)] <- tdata["NormHHCorr",cn[i]]; res2$NormHH[cbind(i2,i1)] <- tdata["NormHHCorr",cn[i]]; 
+    res2$n[cbind(i1,i2)] <- tdata["n_people",cn[i]]; res2$n[cbind(i2,i1)] <- tdata["n_people",cn[i]]; 
+    res2$BestpvalK[cbind(i1,i2)] <- tdata["GenCorrQ_sig",cn[i]]; res2$BestpvalK[cbind(i2,i1)] <- tdata["GenCorrQ_sig",cn[i]]; 
+    res2$BestpvalHH[cbind(i1,i2)] <- tdata["HHCorrQ_sig",cn[i]]; res2$BestpvalHH[cbind(i2,i1)] <- tdata["HHCorrQ_sig",cn[i]]; 
+    res2$cSp[cbind(i1,i2)] <- tdata["modelPheno",cn[i]]; res2$cSp[cbind(i2,i1)] <- tdata["modelPheno",cn[i]]; 
+    res2$Sp[cbind(i1,i2)] <- tdata["Spearman",cn[i]]; res2$Sp[cbind(i2,i1)] <- tdata["Spearman",cn[i]]; 
+    res2$cSpPval[cbind(i1,i2)] <- tdata["modelPheno_pval",cn[i]]; res2$cSpPval[cbind(i2,i1)] <- tdata["modelPheno_pval",cn[i]]; 
+  }  
   #combine
   if(all(is.na(res))){
     res <- tdata
   }else{
     res <- cbind(res,tdata)
   }
-
-  i1 <- as.numeric(match(pheno.1,phenotypes))
-  i2 <- as.numeric(match(pheno.2,phenotypes))
-  
-  res2$K[cbind(i1,i2)] <- tdata["GenCorr",cn[i]]; res2$K[cbind(i2,i1)] <- tdata["GenCorr",cn[i]]; 
-  res2$HH[cbind(i1,i2)] <- tdata["HHCorr",cn[i]]; res2$HH[cbind(i2,i1)] <- tdata["HHCorr",cn[i]]; 
-  res2$NormK[cbind(i1,i2)] <- tdata["NormGenCorr",cn[i]]; res2$NormK[cbind(i2,i1)] <- tdata["NormGenCorr",cn[i]]; 
-  res2$NormHH[cbind(i1,i2)] <- tdata["NormHHCorr",cn[i]]; res2$NormHH[cbind(i2,i1)] <- tdata["NormHHCorr",cn[i]]; 
-  res2$n[cbind(i1,i2)] <- tdata["n_people",cn[i]]; res2$n[cbind(i2,i1)] <- tdata["n_people",cn[i]]; 
-  res2$BestpvalK[cbind(i1,i2)] <- tdata["GenCorrQ_sig",cn[i]]; res2$BestpvalK[cbind(i2,i1)] <- tdata["GenCorrQ_sig",cn[i]]; 
-  res2$BestpvalHH[cbind(i1,i2)] <- tdata["HHCorrQ_sig",cn[i]]; res2$BestpvalHH[cbind(i2,i1)] <- tdata["HHCorrQ_sig",cn[i]]; 
-  res2$cSp[cbind(i1,i2)] <- tdata["modelPheno",cn[i]]; res2$cSp[cbind(i2,i1)] <- tdata["modelPheno",cn[i]]; 
-  res2$Sp[cbind(i1,i2)] <- tdata["Spearman",cn[i]]; res2$Sp[cbind(i2,i1)] <- tdata["Spearman",cn[i]]; 
-  res2$cSpPval[cbind(i1,i2)] <- tdata["modelPheno_pval",cn[i]]; res2$cSpPval[cbind(i2,i1)] <- tdata["modelPheno_pval",cn[i]]; 
-  
 }
 
 if (tcnt<NN){
@@ -347,9 +388,9 @@ for (i in 1:length(ttitle)){ # i=1
   workdirname <- strsplit(config$phenotypes_GCTA_prefix,"/",fixed = T)[[1]]
   workdirname <- paste0(paste0(c(workdirname[1:(length(workdirname)-1)]),collapse = "/"),"/")
   ddata <- data.frame(
-    name=factor(config$phenotypes_fieldnames,levels = rev(config$phenotypes_fieldnames)),
-    value=rep(NA,length(config$phenotypes_fieldnames)),
-    sd=rep(NA,length(config$phenotypes_fieldnames))
+    name=factor(phenotypes_fieldnames,levels = rev(phenotypes_fieldnames)),
+    value=rep(NA,length(phenotypes_fieldnames)),
+    sd=rep(NA,length(phenotypes_fieldnames))
   )
   rownames(ddata) <- ddata$name
   phenotypes <- read.table(paste0(config$phenotypes_GCTA_prefix,".pheno.names"),sep="\t",header = F, row.names = 1)
@@ -363,36 +404,61 @@ for (i in 1:length(ttitle)){ # i=1
     }
     data = readLines(fher)
     
-    rsG1 <- data[which(grepl("V\\(G1)/Vp\t", data))]
-    if(length(rsG1)!=1){
-      print("Something is wrong")
-      break
-    }
-    rsG2 <- data[which(grepl("V\\(G2)/Vp\t", data))]
-    if(length(rsG2)!=1){
-      print("Something is wrong")
-      break
-    }
-    mnG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][2])
-    seG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][3])
-    mnG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][2])
-    seG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][3])
-    if(ttitle[i]=="K.txt"){
-      ddata[phenotypes[j],"value"] <- mnG1
-      ddata[phenotypes[j],"sd"] <- seG1
-    }else if(ttitle[i]=="HH.txt"){
-      ddata[phenotypes[j],"value"] <- mnG2
-      ddata[phenotypes[j],"sd"] <- seG2
+    if(length(config$household_matrix_filename)>0){ #two matrix case
+      rsG1 <- data[which(grepl("V\\(G1)/Vp\t", data))]
+      if(length(rsG1)!=1){
+        print("Something is wrong")
+        break
+      }
+      rsG2 <- data[which(grepl("V\\(G2)/Vp\t", data))]
+      if(length(rsG2)!=1){
+        print("Something is wrong")
+        break
+      }
+      mnG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][2])
+      seG1 <- as.numeric(strsplit(rsG1,"\t",fixed = T)[[1]][3])
+      mnG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][2])
+      seG2 <- as.numeric(strsplit(rsG2,"\t",fixed = T)[[1]][3])
+      if(ttitle[i]=="K.txt"){
+        ddata[phenotypes[j],"value"] <- mnG1
+        ddata[phenotypes[j],"sd"] <- seG1
+      }else if(ttitle[i]=="HH.txt"){
+        ddata[phenotypes[j],"value"] <- mnG2
+        ddata[phenotypes[j],"sd"] <- seG2
+      }else{
+        stop("Unknown parameter")
+      }
     }else{
-      stop("Unknown parameter")
-    }
-  }
+      rsG <- data[which(grepl("V\\(G)\t", data))]
+      if(length(rsG)!=1){
+        print("Something is wrong")
+        break
+      }
+      rsE <- data[which(grepl("V\\(e)\t", data))]
+      if(length(rsE)!=1){
+        print("Something is wrong")
+        break
+      }
+      rsN <- data[which(grepl("n\t", data))]
+      if(length(rsN)!=1){
+        print("Something is wrong")
+        break
+      }
+      mnG <- as.numeric(strsplit(rsG,"\t",fixed = T)[[1]][2])
+      seG <- as.numeric(strsplit(rsG,"\t",fixed = T)[[1]][3])
+      mnE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][2])
+      seE <- as.numeric(strsplit(rsE,"\t",fixed = T)[[1]][3])
+      nn <- as.numeric(strsplit(rsN,"\t",fixed = T)[[1]][2])
+      
+      ddata[phenotypes[j],"value"] <- mnG
+      ddata[phenotypes[j],"sd"] <- seG
+    }    
+  }  
   
   ddata$value[ddata$value < 0] <- 0
   ddata$value[ddata$value > 1] <- 1
   ddata$value[is.na(ddata$value)] <- 0
   ddata$sd[is.na(ddata$sd)] <- 0
-  
   if(ttitle[i]=="K.txt"){
     write.table(ddata,gsub("NormK.txt","K_Heritabilities.txt",fNormK),row.names = F, sep = "\t")
   }else{
